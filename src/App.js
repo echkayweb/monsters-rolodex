ccaf02bfb0cdd069c82d57a970444db7cc3d7ea8
hkxxx843 <hkxxx843@gmail.com>
2021-07-20 19:35:43 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: 0,
+        },
+        {
+          name: "Dracula",
+          id: 1,
+        },
+        {
+          name: "Zombie",
+          id: 2,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
